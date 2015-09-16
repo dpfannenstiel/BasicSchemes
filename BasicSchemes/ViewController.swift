bd07b5fc8f78b1f7470e84501a9ded3f2c74d526
Dustin Pfannenstiel <dustin@nth-estate.com>
2015-09-15 20:34:25 -0400

    project setup

--- BasicSchemes/ViewController.swift
+++ BasicSchemes/ViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        if let arguments = NSProcessInfo.processInfo().arguments as? [String],
+            let indexOfColorFlag = find(arguments, "--Color") where arguments.count > indexOfColorFlag {
+                let colorString = arguments[indexOfColorFlag+1].lowercaseString
+                
+                let color:UIColor
+                
+                switch colorString {
+                    case "blue":
+                        color = UIColor.blueColor()
+                    case "red":
+                        color = UIColor.redColor()
+                    case "green":
+                        color = UIColor.greenColor()
+                    case "yellow":
+                        color = UIColor.yellowColor()
+                    default:
+                        color = UIColor.grayColor()
+                }
+                
+                self.view.backgroundColor = color
+                label.text = colorString.capitalizedString
+                
+        } else {
+            label.text = "No color selected!"
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
